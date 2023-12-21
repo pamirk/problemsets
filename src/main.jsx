e0ffa16454fe62bc9e76971e98518f7c25d40bbb
Pamir khan <pmysteryk@gmail.com>
2023-12-21 17:18:23 +0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
