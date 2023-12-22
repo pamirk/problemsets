ff9bf5e94f08130cbfc1262e628d1ccb54f18f28
Pamir khan <pmysteryk@gmail.com>
2023-12-22 15:42:01 +0500

    add react-router

---
+++ src/Router.jsx
@@ -0,0 +1,17 @@
+// import firebase from "firebase";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router";
+
+export const Router = () => {
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
