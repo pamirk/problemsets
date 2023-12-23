6afff2a331a80f37419f795ca9dd2406abb39b3a
Pamir khan <pmysteryk@gmail.com>
2023-12-23 19:15:58 +0500

    add pyodide to grading page

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,13 +4,16 @@ import {Route, Switch} from "react-router";
 import {HomeRedirect} from "./HomeRedirect.jsx";
 import {Authenticated} from "./components/general/auth/Authenticated";
 import {Home} from "./home/Home.jsx";
+import PyodideProvider from "./components/pyodide/PyodideProvider.jsx";
 
 export const Router = () => {
 
     return (
         <BrowserRouter>
             <Switch>
-
+                <Route path="/grading/:qtrId/:psetId/:qId/:studentId">
+                    <PyodideProvider> <Authenticated component={null}/> </PyodideProvider>
+                </Route>
                 <Route path="/:qtrId">
                     <Authenticated component={Home}/>
                 </Route>
