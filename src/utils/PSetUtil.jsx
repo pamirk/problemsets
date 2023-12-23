69ebffd9f7cf0fc295b1f83706455d7a798b9a06
Pamir khan <pmysteryk@gmail.com>
2023-12-23 19:23:49 +0500

    add getQuestionInfo function for question info from qId

--- src/utils/PSetUtil.jsx
+++ src/utils/PSetUtil.jsx
@@ -1,3 +1,23 @@
+export const getQuestionInfo = function (qId, publicQuestionInfo) {
+    for (var i = 0; i < publicQuestionInfo.length; i++) {
+        let q = publicQuestionInfo[i]
+        if (q['qId'] == qId) {
+            q['index'] = i + 1
+            return q
+        }
+        // the question might have subparts!
+        if (q['subparts']) {
+            for (var j = 0; j < q['subparts'].length; j++) {
+                let s = q['subparts'][j]
+                if (s['qId'] == qId) {
+                    s['index'] = i + 1
+                    return s
+                }
+            }
+        }
+    }
+    return null;
+}
 
 export const isEditorActive = function () {
     let activeClass = document.activeElement.className
