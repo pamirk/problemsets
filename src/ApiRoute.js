0dd47e096578aba0e0f24192adc1bd9fe870c92e
Pamir khan <pmysteryk@gmail.com>
2023-12-25 13:20:18 +0500

    add ApiRoute

---
+++ src/ApiRoute.js
@@ -0,0 +1,6 @@
+
+
+const API_ROUTE = 'https://us-central1-guia-cs109psets.cloudfunctions.net/api/';
+
+// const API_ROUTE = 'http://localhost:5001/guia-cs109psets/us-central1/api/';
+export default API_ROUTE 
