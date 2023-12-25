781da531c1d97fb1d86d3799340cf0584689b484
Pamir khan <pmysteryk@gmail.com>
2023-12-25 12:35:09 +0500

    add colearning constants

---
+++ src/components/colearning/constants.js
@@ -0,0 +1,5 @@
+export const ROOM_WAITING = 'waiting';
+export const ROOM_REJECTED = 'rejected';
+export const ROOM_READY = 'ready';
+export const ROOM_JOINED = 'joined';
+export const ROOM_EXITED = 'exited';
